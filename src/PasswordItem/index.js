e7a0623b5e50705b05e6f1fadbc98ea8e5fae50e
eswar <eshwardadi12@gmail.com>
2023-07-28 07:37:54 +0000

    changesincode

--- src/PasswordItem/index.js
+++ src/PasswordItem/index.js
@@ -34,6 +34,7 @@ const PasswordItem = props => {
         <img
           src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png "
           alt="delete"
+          className="delete-image"
         />
       </button>
     </li>
